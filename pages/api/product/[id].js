0de0311133f49ee89c9258a6df7d07b00fd9107a
chandan kumar <chandanpk9@gmail.com>
2023-09-06 15:26:28 +0530

    containarize with docker and docker-compose

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -1,24 +1,28 @@
-import Product from '../../../models/Product'
-import dbConnect from '../../../utils/mongo'
+import Product from "../../../models/Product";
+import dbConnect from "../../../utils/mongo";
 
 // import mongoose from 'mongoose';
-export default async function handler(req, res){
-    const { method, query: { id } } = req
+export default async function handler(req, res) {
+  const {
+    method,
+    query: { id },
+  } = req;
 
-    await dbConnect();
+  await dbConnect();
 
-    if (method === 'POST') {
-        console.log("Post request was made")
+  if (method === "POST") {
+    console.log("Post request was made");
+  }
+
+  console.log("hittttttttttttttttttttttt");
+
+  if (method === "GET") {
+    try {
+      const data = await Product.findById(id);
+      res.status(200).json(data);
+    } catch (err) {
+      console.log(err.message);
+      // res.status(500).json(err)
     }
-
-    if (method === 'GET') {
-        try {
-            const data = await Product.findById(id)
-            res.status(200).json(data)
-        } catch (err) {
-            console.log(err.message)
-            // res.status(500).json(err)
-        }
-    }
-
+  }
 }
